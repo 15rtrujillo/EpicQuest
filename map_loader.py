a018bfc4933ed61273cd1722347ab0803e5ad930
15rtrujillo <ryantrujillo@comcast.net>
2023-03-09 17:56:12 -0700

    Finished map loading.

--- map_loader.py
+++ map_loader.py
@@ -9,20 +9,37 @@ game_map = dict()
 def load_map_file(map_file_name: str):
     """Load a map file
     map_file_name: The name of the map file to load"""
+
+    # Attempt to open the map file
     map_file = None
     try:
         map_file = open(map_file_name, "r")
     except FileNotFoundError:
-        LogManager.get_logger().error(f"Could not open the provided map file: {map_file_name} - The file does not exist.")
+        LogManager.get_logger().error(f"Could not open map file: {map_file_name} - The file does not exist.")
         return
     
-    map_json = json.loads(map_file.read())
+    # Attempt to load the JSON from file
+    try:
+        map_json = json.loads(map_file.read())
+    except json.JSONDecodeError:
+        LogManager.get_logger().error(f"Error reading map file: {map_file_name} - The file is possibly corrupted.")
+        return
 
-    # Read the JSON into a Room class
-    print(map_json)
+    # Loop through every object in the JSON array to create a new room
+    for json_obj in map_json:
+        new_room = Room()
+
+        # Now loop through every attribute of the JSON object and assign it to a class member
+        for key, value in json_obj.items():
+            if key in new_room.__dict__.keys():
+                new_room.__dict__[key] = value
+
+        # Add the room to the map
+        game_map[new_room.id] = new_room
 
     map_file.close()
 
 
 if __name__ == "__main__":
+    """Try to read the room file dumped from the Room class test to test map loader"""
     load_map_file("room.eqm")
--- room.py
+++ room.py
@@ -1,7 +1,7 @@
 class Room:
     """Holds information about a map location"""
 
-    def __init__(self, id: int, name: str, desc: str, n: int = -1, ne: int = -1, e: int = -1, se: int = -1, s: int = -1, sw: int = -1, w: int = -1, nw: int = -1, up: int = -1, down: int = -1):
+    def __init__(self, id: int = -1, name: str = "", desc: str = "", n: int = -1, ne: int = -1, e: int = -1, se: int = -1, s: int = -1, sw: int = -1, w: int = -1, nw: int = -1, up: int = -1, down: int = -1):
         """Create a new Room with links to attached rooms
         id: The ID of the room
         name: The name of the room
@@ -32,8 +32,8 @@ class Room:
 
 
 if __name__ == "__main__":
-    """Dumps the Room class to a map file"""
+    """Dumps the Room class to a map file for testing"""
     import json
     temp = Room(-1, "", "")
     file = open("room.eqm", "wb")
-    file.write(json.dumps(temp, default=lambda o: o.__dict__, indent=4).encode())
+    file.write(json.dumps([temp], default=lambda o: o.__dict__, indent=4).encode())
