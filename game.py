66bafa5810ebf50d185aa43afbac28261a2d0fc1
Rrrrry123 <ryantrujillo@comcast.net>
2023-03-16 15:18:25 -0600

    More game progress

--- game.py
+++ game.py
@@ -6,26 +6,100 @@ import game_data_loader
 import save_manager
 
 
+player = None
+
+
+def load_game():
+    """Present a list of the saved characters and let the player choose one to load"""
+    saves = save_manager.get_saves()
+    print()
+    if len(saves) > 0:
+        print("Saved Characters:")
+        for save in saves:
+            print(save)
+        print()
+        print("Which save would you like to load?")
+        print("Type the name of a character or press ENTER to go back to the main menu")
+        # We need to be allowed to take the empty string as input
+        saves.append("")
+        response = get_text_input("", saves)
+        if response == "":
+            return
+        
+        # TODO: FINISH ME
+
+    else:
+        print("There are no saves to load")
+        print()
+        pause()
+
+
+def about():
+    """Displays the about section"""
+    print()
+    print("Epic Quest: Text Quest")
+    print("The code is open source and pending licensing")
+    print("The IP of Epic Quest and all things associated with it is Copyright (C) Ryan Trujillo")
+    print()
+    pause()
+
+
+def quit():
+    """Quit the game and save the player"""
+    if player != None:
+        save_manager.save(player)
+    print()
+    print("Thank you for playing Epic Quest.")
+    print("Goodbye!")
+    exit(0)
+    
+
+
 def main_menu():
     """Display the main menu for the game"""
-    print("Epic Quest: Quest for Epicness")
+    print()
+    print("Epic Quest: Text Quest")
     print()
     print("Main Menu")
     print("1. New Game")
     print("2. Load Game")
     print("3. About")
     print("4. Exit")
+    print()
     choice = get_int_input(number_of_choices=4)
+    if choice == 1:
+        pass
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        about()
+    elif choice == 4:
+        quit()
 
 
 def main():
-    print("Epic Quest: Quest for Epicness")
+    print("Epic Quest: Text Quest")
     print()
     print("Loading data...")
 
     # Load all the definitions into the World
     game_data_loader.load_all()
     print("Data loaded!")
+
+
+def get_text_input(prompt: str = "Your answer: ", allowed_responses: list[str] = None) -> str:
+    """Gets string input from the user.
+    prompt: The prompt to be displayed to the user
+    allowed_responses: If this is not None, the user's input will be checked against a list of allowed responses. If the user enters an invalid response, they will be prompted to try again."""
+    while True:
+        response = input(prompt)
+        if allowed_responses == None:
+            return response
+        if response in allowed_responses:
+            return response
+        print()
+        print("Please input a valid option")
+        print()
 
 
 def get_int_input(prompt: str = "Your choice: ", number_of_choices: int = -1) -> int:
@@ -38,9 +112,13 @@ def get_int_input(prompt: str = "Your choice: ", number_of_choices: int = -1) ->
             if int(c) in range(1, number_of_choices+1):
                 return int(c)
             else:
+                print()
                 print("Please input a valid option")
+                print()
         except:
+            print()
             print("Please input a number")
+            print()
 
 
 def pause():
